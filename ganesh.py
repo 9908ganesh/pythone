ceef398a971f7a8d6a8c74a49c502e4df11fe1b1
9908ganesh <157773983+9908ganesh@users.noreply.github.com>
2024-02-08 12:33:29 +0530

    this is basic code of pythone

---
+++ ganesh.py
@@ -0,0 +1,33 @@
+# Define a variable
+x = 10
+
+# Example 1: Simple if statement
+if x > 5:
+    print("x is greater than 5")
+
+# Example 2: if-else statement
+if x % 2 == 0:
+    print("x is even")
+else:
+    print("x is odd")
+
+# Example 3: if-elif-else statement
+if x > 10:
+    print("x is greater than 10")
+elif x < 10:
+    print("x is less than 10")
+else:
+    print("x is equal to 10")
+
+# define a variable
+    a = 40
+
+ #   example 1: simple if statement
+    if a > 30:
+        print("this is true statement")
+  #      example 2:simple if-else statement
+        if a > 40:
+            print("this is false ")
+        else :
+
+            print("this is true")
