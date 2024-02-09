0306037aee18f259dddb252189295b6599835b06
9908ganesh <157773983+9908ganesh@users.noreply.github.com>
2024-02-09 11:24:04 +0530

    arthamatic and relational operators

--- operators.py
+++ operators.py
@@ -21,3 +21,64 @@ print("Division:", result_division)
 # Modulus (Remainder)
 result_modulus = a % b
 print("Modulus:", result_modulus)
+#Arthamatic operators 
+
+#example 2
+a=44
+b=78
+c=24
+d=80
+e=56
+# additions
+print(a+b)
+print (b+c)
+print(c+d)
+print(d+e)
+print(e+a)
+# substractions
+print(a-b)
+print(b-c)
+print(c-d)
+print(d-e)
+print(e-a)
+# multipliction
+print(a*b)
+print(b*c)
+print(c*d)
+print(d*e)
+print(e*a)
+# Divission
+print(a/b)
+print(b/c)
+print(c/d)
+print(d/e)
+print(e/a)
+#modulus
+print(a%b)
+print(b%c)
+print(c%d)
+print(d%e)
+print(e%a)
+# comarison or relational operators 
+
+# Declare two variables
+g = 10
+h = 20
+
+# Greater than
+print("g is greater than h ", g > h) 
+
+# Less than
+print("g is less than h", g < h)  
+
+# Equal to
+print("g is equal h", g == h) 
+
+# Not equal to
+print(" g not equal to h", g != h)  
+
+# Greater than or equal to
+print("g is greater than or equal to h", g >= h) 
+
+# Less than or equal to
+print("g is less than or equal to h", g <= h)
